80e02403ae2300cd08f9855627655dc29755bc3e
JoAustin312 <65620601+JoAustin312@users.noreply.github.com>
2022-05-26 18:09:36 +0100

    added more tests and checks for null values

--- src/main/java/com/sparta/ja/Palindrome.java
+++ src/main/java/com/sparta/ja/Palindrome.java
@@ -1,67 +1,63 @@
 package com.sparta.ja;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Palindrome
 {
     public static void main( String[] args ) {
 
-//        System.out.println(isPalindrome("racecar"));
-//        System.out.println(isPalindrome("helicopter"));
-//        System.out.println(isPalindrome("radar"));
-//
-//        String palindromeStringTest = "ALULA ANANA CIVIC DEKED DELED DERED DEWED KAIAK KAYAK LEMEL LEVEL MADAM MALAM MINIM RADAR REFER ROTOR SAGAS SAMAS " +
-//                "SEDES SELES SEMES SERES SEXES SHAHS SIMIS SIRIS SOLOS STATS STETS STOTS SULUS SUSUS TENET TOROT ARAARA ATAATA kayak deified rotator " +
-//                "repaper deed peep wow noon civic racecar level mom bird rib taco cat UFO tofu Borrow or rob? Never odd or even. We panic in a pew. " +
-//                "Won’t lovers revolt now? Ma is a nun, as I am. Don’t nod. Sir, I demand, I am a maid named Iris. Was it a car or a cat I saw? Yo, " +
-//                "Banana Boy! Eva, can I see bees in a cave? Madam, in Eden, I’m Adam. A man, a plan, a canal, Panama! Never a foot too far, even. " +
-//                "Red roses run no risk, sir, on Nurse’s order. He lived as a devil, eh? Ned, I am a maiden. Now, sir, a war is won! Evade me, Dave! " +
-//                "Dennis and Edna sinned. Step on no pets!";
-//        System.out.println(findLongestPalindrome(palindromeStringTest));
-
-        //Replaced this with actual tests
     }
 
     public static boolean isPalindrome(String wordToCheck){
 
-        String lowerCaseWord = wordToCheck.toLowerCase();
-        String reversed = reverse(lowerCaseWord);
+        if (wordToCheck != null) {
+            String lowerCaseWord = wordToCheck.toLowerCase();
+            String reversed = reverse(lowerCaseWord);
 
-        return reversed.equals(lowerCaseWord);
+            return reversed.equals(lowerCaseWord);
+        } else return false;
     }
 
     public static String reverse(String wordToReverse) {
         StringBuilder reversed = new StringBuilder();
-        int len = wordToReverse.length();
+        if (wordToReverse != null) {
+            int len = wordToReverse.length();
 
-        for (int i = len-1; i >= 0; i--){
-            reversed.append(wordToReverse.charAt(i));
-        }
-        return reversed.toString();
+            for (int i = len-1; i >= 0; i--){
+                reversed.append(wordToReverse.charAt(i));
+            }
+            return reversed.toString();
+        } else return "Null value provided";
     }
 
     public static ArrayList<String> findLongestPalindrome(String sentenceToCheck){
-        String[] splitSentence = sentenceToCheck.replaceAll("\\p{Punct}", "").split(" ");//removes punctuation and splits string into array
-        ArrayList<String> listOfPalindromes = new ArrayList<>();
-        ArrayList<String> listOfLongestPalindromes = new ArrayList<>();
-        int longestWordChars = 0;
+        if (sentenceToCheck != null) {
+            String[] splitSentence = sentenceToCheck.replaceAll("\\p{Punct}", "").split(" ");//removes punctuation and splits string into array
+            ArrayList<String> listOfPalindromes = new ArrayList<>();
+            ArrayList<String> listOfLongestPalindromes = new ArrayList<>();
+            int longestWordChars = 0;
 
-        for (String word : splitSentence){ //checks word is more than 3 chars and is a palindrome then adds to arraylist
-            if (word.length() >= 3 && isPalindrome(word)){
-                listOfPalindromes.add(word);
+            for (String word : splitSentence){ //checks word is more than 3 chars and is a palindrome then adds to arraylist
+                if (word.length() >= 3 && isPalindrome(word)){
+                    listOfPalindromes.add(word);
+                }
             }
-        }
-        for (String word : listOfPalindromes){ //finds length of longest word and assigns to variable
-            if (word.length() > longestWordChars){
-                longestWordChars = word.length();
+            for (String word : listOfPalindromes){ //finds length of longest word and assigns to variable
+                if (word.length() > longestWordChars){
+                    longestWordChars = word.length();
+                }
             }
-        }
-        for (String word : listOfPalindromes){ //adds words to list that match longest length
-            if (word.length() == longestWordChars){
-                listOfLongestPalindromes.add(word);
+            for (String word : listOfPalindromes){ //adds words to list that match longest length
+                if (word.length() == longestWordChars){
+                    listOfLongestPalindromes.add(word);
+                }
             }
+            return listOfLongestPalindromes;
+        } else {
+            return new ArrayList<>(Collections.singletonList("Null value provided"));
         }
-        return listOfLongestPalindromes;
     }
 
 }
--- src/test/java/com/sparta/ja/PalindromeTest.java
+++ src/test/java/com/sparta/ja/PalindromeTest.java
@@ -1,29 +1,42 @@
 package com.sparta.ja;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+import static com.sparta.ja.Palindrome.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class PalindromeTest
 {
     @Test
     @DisplayName("should return true racecar")
     public void shouldReturnTrueRacecar() {
-        Assertions.assertTrue(Palindrome.isPalindrome("racecar"));
+        assertTrue(isPalindrome("racecar"));
     }
+
     @Test
     @DisplayName("should return true helicopter")
     public void shouldReturnFalseHelicopter() {
-        Assertions.assertFalse(Palindrome.isPalindrome("helicopter"));
+        assertFalse(isPalindrome("helicopter"));
     }
+
     @Test
     @DisplayName("should return true radar")
-    public void shouldReturnFalseRadar() {
-        Assertions.assertTrue(Palindrome.isPalindrome("radar"));
+    public void shouldReturnTrueRadar() {
+        assertTrue(isPalindrome("radar"));
     }
+
+    @Test
+    @DisplayName("should return reversed bottle")
+    public void shouldReturnReversedBottle() { assertEquals("elttob", reverse("bottle"));}
+
+    @Test
+    @DisplayName("should return reversed speaker")
+    public void shouldReturnReversedSpeaker() { assertEquals("rekaeps", reverse("speaker"));}
+
     @Test
     @DisplayName("should return list of longest palindromes")
     public void shouldReturnListOfLongestPalindromes() {
@@ -31,10 +44,22 @@ public class PalindromeTest
                 "SEDES SELES SEMES SERES SEXES SHAHS SIMIS SIRIS SOLOS STATS STETS STOTS SULUS SUSUS TENET TOROT ARAARA ATAATA kayak deified rotator " +
                 "repaper deed peep wow noon civic racecar level mom bird rib taco cat UFO tofu Borrow or rob? Never odd or even. We panic in a pew. " +
                 "Won’t lovers revolt now? Ma is a nun, as I am. Don’t nod. Sir, I demand, I am a maid named Iris. Was it a car or a cat I saw? Yo, " +
-                "Banana Boy! Eva, can I see bees in a cave? Madam, in Eden, I’m Adam. A man, a plan, a canal, Panama! Never a foot too far, even. " +
-                "Red roses run no risk, sir, on Nurse’s order. He lived as a devil, eh? Ned, I am a maiden. Now, sir, a war is won! Evade me, Dave! " +
-                "Dennis and Edna sinned. Step on no pets!";
+                "Banana Boy! Eva, can I see bees in a cave? Madam, in Eden, I’m Adam. A man, a plan, a canal, Panama! Never a foot too far, even.";
         ArrayList<String> expectedResult = new ArrayList<>(Arrays.asList("deified", "rotator", "repaper", "racecar"));
-        Assertions.assertArrayEquals(expectedResult.toArray(), Palindrome.findLongestPalindrome(palindromeStringTest).toArray());
+        assertArrayEquals(expectedResult.toArray(), findLongestPalindrome(palindromeStringTest).toArray());
     }
+
+    @Test
+    @DisplayName("should return false from isPalindrome null value")
+    public void shouldReturnFalseFromIsPalindromeNullValue() { assertFalse(isPalindrome(null));}
+
+    @Test
+    @DisplayName("should return null message from reverse")
+    public void shouldReturnNullMessageFromReverse() { assertEquals("Null value provided", reverse(null));}
+
+    @Test
+    @DisplayName("should return array with null message from findLongestPalindrome")
+    public void shouldReturnArrayWithNullMessageFromFindLongestPalindrome() {
+        ArrayList<String> expected = new ArrayList<>(Collections.singletonList("Null value provided"));
+        assertArrayEquals(expected.toArray(), findLongestPalindrome(null).toArray());}
 }
